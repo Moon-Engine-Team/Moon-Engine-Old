c2dfea4fc76096d86947041f204bf95e45db0240
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2023-09-06 22:51:12 -0200

    Add files via upload

--- source/states/editors/MasterEditorMenu.hx
+++ source/states/editors/MasterEditorMenu.hx
@@ -16,6 +16,7 @@ class MasterEditorMenu extends MusicBeatState
 	var options:Array<String> = [
 		'Chart Editor',
 		'Character Editor',
+		'Stage Editor (BETA)',
 		'Week Editor',
 		'Menu Character Editor',
 		'Dialogue Editor',
@@ -113,10 +114,12 @@ class MasterEditorMenu extends MusicBeatState
 		if (controls.ACCEPT)
 		{
 			switch(options[curSelected]) {
-				case 'Chart Editor'://felt it would be cool maybe
+				case 'Chart Editor': // felt it would be cool maybe
 					LoadingState.loadAndSwitchState(new ChartingState(), false);
 				case 'Character Editor':
 					LoadingState.loadAndSwitchState(new CharacterEditorState(Character.DEFAULT_CHARACTER, false));
+			    case 'Stage Editor (BETA)': // i'll finish it somedays... maybe....
+					LoadingState.loadAndSwitchState(new StageEditorState(), false);
 				case 'Week Editor':
 					MusicBeatState.switchState(new WeekEditorState());
 				case 'Menu Character Editor':
