94327c811a41bd6684c7ebd21eede992bfd78d65
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2023-11-06 11:30:52 -0300

    Add files via upload

--- source/psychlua/HScript.hx
+++ source/psychlua/HScript.hx
@@ -224,7 +224,7 @@ class HScript extends SScript
 		return callValue;
 	}
 
-	public function executeFunction(funcToRun:String = null, funcArgs:Array<Dynamic>):SCall
+	public function executeFunction(funcToRun:String = null, funcArgs:Array<Dynamic>):TeaCall
 	{
 		if (funcToRun == null)
 			return null;
@@ -236,7 +236,7 @@ class HScript extends SScript
 	{
 		#if LUA_ALLOWED
 		funk.addLocalCallback("runHaxeCode", function(codeToRun:String, ?varsToBring:Any = null, ?funcToRun:String = null, ?funcArgs:Array<Dynamic> = null):Dynamic {
-			var retVal:SCall = null;
+			var retVal:TeaCall = null;
 			#if (SScript >= "3.0.0")
 			initHaxeModule(funk);
 			funk.hscript.doString(codeToRun);
